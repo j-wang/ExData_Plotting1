8d87d59496638192c0b75f5403ce568a7a2acace
James Wang <veriathon@gmail.com>
2014-10-10 15:43:14 -0700

    Commit of plotted content.

---
+++ plot1.R
@@ -0,0 +1,20 @@
+# Read in data
+data_path <- file.path("data/household_power_consumption.txt")
+header <- read.table(data_path, nrows=1, header=FALSE, sep=";", stringsAsFactors=FALSE)
+dat <- read.table(data_path, nrows=69517-66638+1, skip=66637, header=FALSE, sep=";", 
+                  stringsAsFactors=FALSE, colClasses=c("character", "character", rep("numeric",7)))
+
+# Format date/time as datetime and add to frame
+dates <- strptime(paste(dat$V1, dat$V2), format="%d/%m/%Y %H:%M:%S")
+colnames(dat) <- unlist(header)
+final_dat <- as.data.frame(dat)
+final_dat$datetime <- dates
+
+# Remove old date and time columns
+final_dat$Date <- NULL
+final_dat$Time <- NULL
+
+# Make plot1.png
+png(file="plot1.png", width=480, height=480)
+hist(final_dat$Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,35 @@
+# Read in data
+data_path <- file.path("data/household_power_consumption.txt")
+header <- read.table(data_path, nrows=1, header=FALSE, sep=";", stringsAsFactors=FALSE)
+dat <- read.table(data_path, nrows=69517-66638+1, skip=66637, header=FALSE, sep=";", 
+                  stringsAsFactors=FALSE, colClasses=c("character", "character", rep("numeric",7)))
+
+# Format date/time as datetime and add to frame
+dates <- strptime(paste(dat$V1, dat$V2), format="%d/%m/%Y %H:%M:%S")
+colnames(dat) <- unlist(header)
+final_dat <- as.data.frame(dat)
+final_dat$datetime <- dates
+
+# Remove old date and time columns
+final_dat$Date <- NULL
+final_dat$Time <- NULL
+
+# Make plot4.png
+png(file="plot4.png", width=480, height=480)
+par(mfrow=c(2,2))  # settings to have 4 plots together
+# First Plot
+plot(x=final_dat$datetime, y=final_dat$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+
+# Second Plot
+plot(x=final_dat$datetime, y=final_dat$Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+# Third Plot
+plot(x=final_dat$datetime, y=final_dat$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(x=final_dat$datetime, y=final_dat$Sub_metering_2, col="red")
+lines(x=final_dat$datetime, y=final_dat$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, bty="n")
+
+# Fourth Plot
+plot(x=final_dat$datetime, y=final_dat$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
